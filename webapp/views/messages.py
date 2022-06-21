66ccd194c0d1a16743de9be3b530d4280b933fdb
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-06-21 18:38:08 +0600

    create 'messages.py'

---
+++ webapp/views/messages.py
@@ -0,0 +1,8 @@
+LEN_ERRORE_MESSAGE = "The amount of integers should be equal to 4"
+UNIQUE_ERRORE_MESSAGE = "The value should be unique"
+RANGE_ERRORE_MESSAGE = "Numbers should be greater than 1 and less than 10"
+INT_ERRORE_MESSAGE = "The value should be integers"
+
+WIN_MESSAGE = "Winner!"
+MISSED_MESSAGE = "No identical numbers"
+RESULT_MESSAGE = "You got {bulls} bulls and {cows} cows"
